4c63c5489b4085dbb9f63c6b50f0f09917017cf1
Pesy Wu <GamerNoTitle@outlook.com>
2023-06-28 03:51:55 +0000

    Added KC display on info

--- utils/GetPlayer.py
+++ utils/GetPlayer.py
@@ -78,6 +78,7 @@ class player:
         try:
             self.vp = data['Balances']['85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741']
             self.rp = data['Balances']['e59aa87c-4cbf-517a-5983-6e81511be9b7']
+            self.kc = data['Balances']['85ca954a-41f2-ce94-9b45-8ca3dd39a00d']
         except KeyError:
             self.auth = False
         self.wallet = data
